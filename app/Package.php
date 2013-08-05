581a4d026f318279804b408b39c5f189b4e84fe5
Vlasta Neubauer <paranoiq@centrum.cz>
2013-08-05 22:30:16 +0200

    Package: repository and service registration; fixed presenters registration

--- app/Package.php
+++ app/Package.php
@@ -3,7 +3,8 @@
 namespace Package;
 
 use Package\Router;
-use Nette;
+use Nette\Configurator;
+use Nette\DI\Container;
 
 
 class Package
@@ -12,18 +13,48 @@ class Package
 	/**
 	 * Registrace balíčku do konfigurátoru
 	 *
-	 * @param Nette\Configurator
+	 * @param Configurator
 	 */
-	public static function register(Nette\Configurator $configurator)
+	public static function register(Configurator $configurator)
 	{
-		/** @var Nette\DI\Container $container */
-		$configurator->onAfter[] = function ($container) {
+		/** @var Container $container */
+		$configurator->onAfter[] = function (Container $container) {
+
 			// registrace rout
 			$container->router[] = new Router($container);
 
 			// registrace jmenného prostoru presenterů
-			$container->presenterFactory->registerNamespace(__NAMESPACE__);
+			$container->getService('nette.presenterFactory')->registerNamespace(__NAMESPACE__);
+
+			// registrace repositářů
+			//self::registerRepository($container, 'name', __NAMESPACE__ . '\\RepositoryClass');
+
+			// registrace služeb
+			//self::registerService($container, 'name', 'Service\\Class\\Name'); // třída
+			//self::registerService($container, 'name', $container->createInstance('Service\\Class\\Name')); // autowiring
 		};
+	}
+
+	/**
+	 * Registruje repozitář do ORM containeru
+	 */
+	public static function registerRepository(Container $container, $name, $class)
+	{
+		if (!$container->orm->isRepository($name))
+		{
+			$container->orm->register($name, $class);
+		}
+	}
+
+	/**
+	 * Registruje službu do DI containeru
+	 */
+	public static function registerService(Container $container, $name, $service)
+	{
+		if (!$container->hasService($name))
+		{
+			$container->addService($name, $service);
+		}
 	}
 
 }
